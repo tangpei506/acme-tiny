d4ea27dfc472273776aff921af8eb7ff8f3b2935
DNARoma <tangpei506@users.noreply.github.com>
2016-07-11 22:49:40 +0200

    Update acme_tiny.py

--- acme_tiny.py
+++ acme_tiny.py
@@ -112,15 +112,16 @@ def get_crt(account_key, csr, acme_dir, log=LOGGER, CA=DEFAULT_CA):
             wellknown_file.write(keyauthorization)
 
         # check that the file is in place
-        wellknown_url = "http://{0}/.well-known/acme-challenge/{1}".format(domain, token)
-        try:
-            resp = urlopen(wellknown_url)
-            resp_data = resp.read().decode('utf8').strip()
-            assert resp_data == keyauthorization
-        except (IOError, AssertionError):
-            os.remove(wellknown_path)
-            raise ValueError("Wrote file to {0}, but couldn't download {1}".format(
-                wellknown_path, wellknown_url))
+        # no use and cause error
+        #wellknown_url = "http://{0}/.well-known/acme-challenge/{1}".format(domain, token)
+        #try:
+        #    resp = urlopen(wellknown_url)
+        #    resp_data = resp.read().decode('utf8').strip()
+        #    assert resp_data == keyauthorization
+        #except (IOError, AssertionError):
+        #    os.remove(wellknown_path)
+        #    raise ValueError("Wrote file to {0}, but couldn't download {1}".format(
+        #        wellknown_path, wellknown_url))
 
         # notify challenge are met
         code, result = _send_signed_request(challenge['uri'], {
